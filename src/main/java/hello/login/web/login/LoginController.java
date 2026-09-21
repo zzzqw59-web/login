23b4edeb64456dcde31e8ab35901458511cf1bae
Clariatinet <zzzqw59@gmail.com>
2026-09-21 23:23:00 +0900

    study: login 처

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -2,20 +2,20 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.login.Member;
+import hello.login.domain.member.MemberRepository;
+import hello.login.web.SessionConst;
 import hello.login.web.login.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginController {
     private final LoginService loginService;
     private final SessionManager sessionManager;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm loginForm) {
@@ -51,8 +52,30 @@ public class LoginController {
 //        return "redirect:/";
 //    }
 
+//    @PostMapping("/login")
+//    public String loginV2(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
+//        if (bindingResult.hasErrors()) {
+//            return "/login/loginForm";
+//        }
+//
+//        Member loginMember = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
+//        log.info("login = {}", loginMember);
+//
+//        if (loginMember == null) {
+//            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 일치하지 않습니다.");
+//            return "/login/loginForm";
+//        }
+//
+//        // 로그인 성공 처리
+//
+//        // 세션 관리자를 통해 세션을 생성하고, 회원 데이터를 보관
+//        sessionManager.createSession(loginMember, response);
+//
+//        return "redirect:/";
+//    }
+
     @PostMapping("/login")
-    public String loginV2(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
+    public String loginV3(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "/login/loginForm";
         }
@@ -66,9 +89,12 @@ public class LoginController {
         }
 
         // 로그인 성공 처리
-
+        // 세션이 있으면 세션 반환, 없으면 신규 세션 생
+        HttpSession session = request.getSession();
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         // 세션 관리자를 통해 세션을 생성하고, 회원 데이터를 보관
-        sessionManager.createSession(loginMember, response);
+//        sessionManager.createSession(loginMember, response);
 
         return "redirect:/";
     }
@@ -79,9 +105,18 @@ public class LoginController {
 //        return "redirect:/";
 //    }
 
+//    @PostMapping("/logout")
+//    public String logoutV2(HttpServletRequest request) {
+//        sessionManager.expire(request);
+//        return "redirect:/";
+//    }
+
     @PostMapping("/logout")
-    public String logoutV2(HttpServletRequest request) {
-        sessionManager.expire(request);
+    public String logoutV3(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
         return "redirect:/";
     }
 
---
+++ src/main/java/hello/login/web/member/Member.java
@@ -0,0 +1,16 @@
+package hello.login.web.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    private Long id;
+    private String loginId;
+    private String password;
+}
